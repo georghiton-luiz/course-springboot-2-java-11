799816fb2cad8810b994d559655c8430cbf8d7bf
georghiton-luiz <georghiton@hotmail.com>
2021-06-16 15:48:15 -0300

    Product

---
+++ src/main/java/com/projetoweb/course/repositories/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.projetoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.projetoweb.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+}
